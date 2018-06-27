8110692e66156195c246c7c91d280bd8c3564e10
YI <differentialmanifold@gmail.com>
2018-06-27 20:57:21 +0800

    apply change

--- flappybird/game/wrapped_flappy_bird.py
+++ flappybird/game/wrapped_flappy_bird.py
@@ -9,8 +9,8 @@ import pygame.surfarray as surfarray
 from pygame.locals import *
 from itertools import cycle
 
-# import os
-# os.environ['SDL_VIDEODRIVER'] = 'dummy'
+import os
+os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
 FPS = 30
 SCREENWIDTH = 288
