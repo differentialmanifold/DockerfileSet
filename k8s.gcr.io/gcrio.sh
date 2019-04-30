557dc98f60f39dbe1ba1201071abd67d45bbc6eb
differentialmanifold <differentialmanifold@gmail.com>
2019-04-30 11:33:27 +0800

    add build gcr.io bash

---
+++ k8s.gcr.io/gcrio.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+# Check version in https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
+# Search "Running kubeadm without an internet connection"
+# For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
+KUBE_VERSION=v1.14.1
+KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.3.10
+DNS_VERSION=1.3.1
+GCR_URL=k8s.gcr.io
+MIRRORS_URL=ccr.ccs.tencentyun.com/dy_tencent_hub
+
+images=(kube-apiserver:${KUBE_VERSION}
+kube-controller-manager:${KUBE_VERSION}
+kube-scheduler:${KUBE_VERSION}
+kube-proxy:${KUBE_VERSION}
+pause:${KUBE_PAUSE_VERSION}
+etcd:${ETCD_VERSION}
+coredns:${DNS_VERSION})
+
+for imageName in ${images[@]} ; do
+  docker pull $MIRRORS_URL/$imageName
+  docker tag  $MIRRORS_URL/$imageName $GCR_URL/$imageName
+  docker rmi $MIRRORS_URL/$imageName
+done
+
+docker images
