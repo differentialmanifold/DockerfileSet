9429ef284ead60271f7aa3634f39b607c29f086b
differentialmanifold <differentialmanifold@gmail.com>
2019-05-09 15:17:36 +0800

    add nfs

--- k8s.gcr.io/gcrio.sh
+++ k8s.gcr.io/gcrio.sh
@@ -7,6 +7,7 @@ KUBE_VERSION=v1.14.1
 KUBE_PAUSE_VERSION=3.1
 ETCD_VERSION=3.3.10
 DNS_VERSION=1.3.1
+VOLUME_NFS=0.8
 GCR_URL=k8s.gcr.io
 MIRRORS_URL=ccr.ccs.tencentyun.com/dy_tencent_hub
 
@@ -16,7 +17,8 @@ kube-scheduler:${KUBE_VERSION}
 kube-proxy:${KUBE_VERSION}
 pause:${KUBE_PAUSE_VERSION}
 etcd:${ETCD_VERSION}
-coredns:${DNS_VERSION})
+coredns:${DNS_VERSION}
+volume-nfs:${VOLUME_NFS})
 
 for imageName in ${images[@]} ; do
   docker pull $MIRRORS_URL/$imageName
